12971c35761836ae25537082bf99fd9b2c3da0a4
Fabiano Parente <fabiano@nunes.me>
2025-06-08 23:13:32 -0300

    compatibiliza com 3.1.42

--- build/post.js
+++ build/post.js
@@ -5,21 +5,6 @@
 var __ffmpegjs_running = false;
 
 self.onmessage = function(e) {
-  function makeOutHandler(cb) {
-    var buf = [];
-    return function(ch, exit) {
-      if (exit && buf.length) return cb(__ffmpegjs_utf8ToStr(buf, 0));
-      if (ch === 10 || ch === 13) {
-        cb(__ffmpegjs_utf8ToStr(buf, 0));
-        buf = [];
-      } else if (ch !== 0) {
-        // See <https://github.com/kripken/emscripten/blob/1.34.4/
-        // src/library_tty.js#L146>.
-        buf.push(ch);
-      }
-    };
-  }
-
   var msg = e.data;
   if (msg["type"] == "run") {
     if (__ffmpegjs_running) {
@@ -38,16 +23,15 @@ self.onmessage = function(e) {
         // via Web Worker message interface, set stdin to no-op. We are
         // messing with other handlers anyway.
       };
-      opts["stdout"] = makeOutHandler(function(line) {
+      opts["print"] = function(line) {
         self.postMessage({"type": "stdout", "data": line});
-      });
-      opts["stderr"] = makeOutHandler(function(line) {
-        self.postMessage({"type": "stderr", "data": line});
-      });
+      }
+      opts["printErr"] = function(line) {
+        line.split("\r").forEach(function(line) {
+          self.postMessage({"type": "stderr", "data": line});
+        });
+      }
       opts["onExit"] = function(code) {
-        // Flush buffers.
-        opts["stdout"](0, true);
-        opts["stderr"](0, true);
         self.postMessage({"type": "exit", "data": code});
       };
       // TODO(Kagami): Should we wrap this function into try/catch in
