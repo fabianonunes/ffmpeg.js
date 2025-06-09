693c5b8adc30ce05a45934d82ac0489b3fc310cd
fabianonunes <fparente@senado.leg.br>
2025-06-09 07:28:35 -0300

    simplifica post

--- build/post.js
+++ build/post.js
@@ -4,45 +4,55 @@
 
 var __ffmpegjs_running = false;
 
+function emit(type, data) {
+  self.postMessage({"type": type, "data": data});
+}
+
 self.onmessage = function(e) {
   var msg = e.data;
-  if (msg["type"] == "run") {
-    if (__ffmpegjs_running) {
-      self.postMessage({"type": "error", "data": "already running"});
-    } else {
-      __ffmpegjs_running = true;
-      self.postMessage({"type": "run"});
-      var opts = {};
-      Object.keys(msg).forEach(function(key) {
-        if (key !== "type") {
-          opts[key] = msg[key]
-        }
-      });
-      opts["stdin"] = function() {
-        // NOTE(Kagami): Since it's not possible to pass stdin callback
-        // via Web Worker message interface, set stdin to no-op. We are
-        // messing with other handlers anyway.
-      };
-      opts["print"] = function(line) {
-        self.postMessage({"type": "stdout", "data": line});
-      }
-      opts["printErr"] = function(line) {
-        line.split("\r").forEach(function(line) {
-          self.postMessage({"type": "stderr", "data": line});
-        });
-      }
-      opts["onExit"] = function(code) {
-        self.postMessage({"type": "exit", "data": code});
-      };
-      opts["done"] = function (result) {
-        __ffmpegjs_running = false;
-        self.postMessage({"type": "done", "data": result}, transfer);
-      }
-      __ffmpegjs(opts);
+
+  if (msg["type"] !== "run")
+    return emit("error", "unknown command");
+
+  if (__ffmpegjs_running)
+    return emit("error", "already running");
+
+  emit("run");
+  __ffmpegjs_running = true;
+
+  var opts = {};
+  Object.keys(msg).forEach(function(key) {
+    if (key !== "type") {
+      opts[key] = msg[key]
     }
-  } else {
-    self.postMessage({"type": "error", "data": "unknown command"});
+  });
+
+  opts["stdin"] = function() {
+    // NOTE(Kagami): Since it's not possible to pass stdin callback
+    // via Web Worker message interface, set stdin to no-op. We are
+    // messing with other handlers anyway.
+  };
+
+  opts["print"] = function(line) {
+    emit("stdout", line);
   }
+
+  opts["printErr"] = function(line) {
+    line.split("\r").forEach(function(line) {
+      emit("stderr", line);
+    });
+  }
+
+  opts["onExit"] = function(code) {
+    emit("exit", code);
+  };
+
+  opts["done"] = function (_result) {
+    __ffmpegjs_running = false;
+    emit("done");
+  }
+
+  __ffmpegjs(opts);
 };
 
 self.postMessage({"type": "ready"});
